fa629c8ccd2a04ce5317076f94304b17110cd7e8
Jesse-Richard <jesse.mulwa@gmail.com>
2025-02-19 10:41:31 +0300

    Pushing Updates

---
+++ pipeline/2cleaning/validation.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+
+
+## Function declarations
+
+def parseTemp(entry):
+    return
+
+def parseRainfall(entry):
+    return
+
+def parseVis(entry):
+    return
+
+def parseSLP(entry):
+    return
+
+def parseDew(entry):
+    return
+
+def parseWindSpeed(entry):
+    return
+
+def parseWindDir(entry):
+    return
+
+
+
+## staging
+
+filepath = '../_downloads/10640099999.csv_combined.csv'
+
+data = pd.read_csv(filepath)
+
+
+
+## parsing block
+
+
+## return filtered data as csv
+
